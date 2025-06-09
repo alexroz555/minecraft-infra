afad180ccb4802d76ee68c180c404e56527f15a0
alexroz555 <rozanova@goaway.>
2025-06-09 16:48:20 -0700

    changed perms

--- ansible/run-script.sh
+++ ansible/run-script.sh
@@ -6,6 +6,6 @@ INSTANCE_IP=$(aws ec2 describe-instances \
   --output text)
 
 echo "[minecraft]" > hosts.ini
-echo "$INSTANCE_IP ansible_user=ec2-user ansible_ssh_private_key_file=~"wordpress.pem >> hosts.ini
+echo "$INSTANCE_IP ansible_user=ec2-user ansible_ssh_private_key_file="wordpress.pem >> hosts.ini
 
 ansible-playbook -i hosts.ini playbook.yml
